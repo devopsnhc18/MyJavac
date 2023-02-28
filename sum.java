da88408e195fdad3768b599e0d4ec25d7ac62d8d
devopsnhc18 <126224104+devopsnhc18@users.noreply.github.com>
2023-02-28 11:21:13 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int x = 89, y = 43;
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is " + sum);
+    }
+}
